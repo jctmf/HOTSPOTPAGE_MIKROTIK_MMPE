6a6e89b0b45f0822e1b503f353755d883547c2cd
João Carlos T. de Melo Filho <jcarlos.teixeira@gmail.com>
2023-08-19 17:57:05 -0300

    Versão Inicial

---
+++ login-soft.js
@@ -0,0 +1,26 @@
+var Login = function () {
+
+	var handleLogin = function() {
+
+	}
+
+return {
+
+        init: function () {
+        	
+            handleLogin();
+    
+
+            $.backstretch([
+		        "bg1.jpg",
+			"bg2.jpg",
+			"bg3.jpg",
+			"bg4.jpg",
+			"bg5.jpg",
+		        ], {duration: 3000, fade: 750});
+	       
+        }
+
+    };
+
+}();
